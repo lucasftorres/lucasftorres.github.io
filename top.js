677fb3fd2d054830917f7b17352acd8d58457291
Lucas Torres <lucas.torresf192@gmail.com>
2024-06-24 00:36:06 -0300

    Amazon Project 1.0

--- top.js
+++ top.js
@@ -21,32 +21,16 @@ const conteudo =
                 "<li>"+
                 "<a href='/page-bi.html'>Business Intelligence</a>"+
                 "<ul>"+
-                "<li><a href='#'>Microsoft Power BI</a>"+
-                "<ul>"+
-                "<li><a href='/pages-projects/bi/powerbi/foodgood/foodgood.html'>Food Good</a></li>"+
-                "<li><a href='/pages-projects/bi/powerbi/global_AI_Salaries/globalAISalaries.html'>Global AI Salaries</a></li>"+
-                "<li><a href='/pages-projects/bi/powerbi/olist/olist.html'>Olist</a></li>"+
-                "<li><a href='/pages-projects/bi/powerbi/waze/waze.html'>Waze</a></li>"+
-                "</ul>"+
-                " </li>"+
-                "<li><a href='#'>Tableau</a>"+
-                "<ul>"+
-                "<li><a href='/pages-projects/bi/tableau/covid19/covid19.html'>Covid 19</a></li>"+
-                "<li><a href='/pages-projects/bi/tableau/house_sales/house_sales.html'>Washington House Sales</a></li>"+
-                "</ul>"+
-                "</li>"+
-                "<li><a href='#'>Looker Studio</a>"+
-                "<ul>"+
-                "<li><a href='/pages-projects/bi/looker/amazon/amazon.html'>Amazon</a></li>"+
-                "<li><a href='/pages-projects/bi/looker/popular_market/popularMarket.html'>Popular Market</a></li>"+
-                "</ul>"+
-                "</li>"+
-                "<li>"+
-                "<a href='#'>Microsoft Excel</a>"+
-                "<ul>"+
-                "<li><a href='/pages-projects/bi/excel_projects/bikesales/bikeSales.html'>Bike Sales</a></li>"+
-                "<li><a href='/pages-projects/bi/excel_projects/coffee/coffee.html'>Coffee</a></li>"+
-                "</ul>"+
+                "<li><a href='/pages-projects/bi/foodgood/foodgood.html'>Food Good</a></li>"+
+                "<li><a href='/pages-projects/bi/global_AI_Salaries/globalAISalaries.html'>Global AI Salaries</a></li>"+
+                "<li><a href='/pages-projects/bi/olist/olist.html'>Olist</a></li>"+
+                "<li><a href='/pages-projects/bi/waze/waze.html'>Waze</a></li>"+
+                "<li><a href='/pages-projects/bi/amazon/amazon.html'>Amazon</a></li>"+
+                "<li><a href='/pages-projects/bi/popular_market/popularMarket.html'>Popular Market</a></li>"+
+                "<li><a href='/pages-projects/bi/bikesales/bikeSales.html'>Bike Sales</a></li>"+
+                "<li><a href='/pages-projects/bi/coffee/coffee.html'>Coffee</a></li>"+
+                "<li><a href='/pages-projects/bi/covid19/covid19.html'>Covid 19</a></li>"+
+                "<li><a href='/pages-projects/bi/house_sales/house_sales.html'>Washington House Sales</a></li>"+
                 "</li>"+
                 "</ul>"+
                 "</li>"+
